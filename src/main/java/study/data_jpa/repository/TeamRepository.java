c6f41c99e746abcad545faa63e95d4fcea73b032
koojahyeob <gjm01083145028@gmail.com>
2025-03-07 16:29:23 +0900

    Revert "first commit about data jpa"
    This reverts commit 20a499417f731d3e2f08c7cd5d30060ea9dad517.

--- src/main/java/study/data_jpa/repository/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package study.data_jpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import study.data_jpa.entity.Team;
-
-// Spring Data JPA가 자동으로 다 상속받아서 기능들 수행한다.
-public interface TeamRepository extends JpaRepository<Team, Long> { //<Entity 이름, entity에 mapping된 PK의 type
-}
